482685cca0823f95d15687a50e34c8e31e08e6f6
Mochineko <t.o.e.4315@gmail.com>
2019-04-21 16:43:59 +0900

    Try to create attribute and its sample

---
+++ Assets/Samples/Scripts/AttributeSampleComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mochineko.ReorderableList.Samples
+{
+	public class AttributeSampleComponent : MonoBehaviour
+	{
+		[SerializeField]
+		[ReorderableList]
+		private List<SampleElement> sampleList = new List<SampleElement> { };
+	}
+}
